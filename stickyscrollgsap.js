3df78c56e76b6488195a02f0a33a4379837c3c64
inqdesign <163176437+inqdesign@users.noreply.github.com>
2024-03-21 12:24:29 +0100

    Update stickyscrollgsap.js

--- stickyscrollgsap.js
+++ stickyscrollgsap.js
@@ -1,33 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
+import { gsap } from 'https://cdn.skypack.dev/gsap';
+import { ScrollTrigger } from 'https://cdn.skypack.dev/gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const createScroll01 = () => {
+  const panels = Array.from(document.querySelectorAll('.sticky-section_panel'));
+
+  panels.forEach((panel, index) => {
+    const isLast = index === panels.length - 1;
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: panel,
+          start: 'top top',
+          scrub: 1,
+        },
+      })
+      // Animate panel
+      .to(
+        panel,
+        {
+          ease: 'none',
+          startAt: { filter: 'brightness(100%) blur(0px)' },
+          filter: isLast ? 'none' : 'brightness(50%) blur(10px)',
+          scale: 0.9,
+          borderRadius: 40,
+        },
+        '<'
+      );
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
   createScroll01();
 });
-  //select Elements from DOM
-  const panels = Array.from(document.querySelectorAll('.sticky-hero_panel'));
-
-           
-  // Funcial to create the scroll trigger animation
-  const createScroll01 = () => {
-    panel.forEach((panel, index)) => {
-      const is Last = index === panels.length - 1;
-
-      gsap
-       .timeline({
-         scrollTrigger: {
-           trigger: panel,
-           start: 'top top',
-           scrub: 1,
-         },
-       })
-      // Animate panel
-      .to
-        (panel,
-         {
-           esae: 'none',
-           startAt: { filter: 'brightness(100%) blur(0px)' },
-           filter: isLsat ? 'none' : 'bringlness(50%) blur(10px)',
-           scale: 0.9,
-         },
-         '<'
-        );
-      });
-};
