4cdea669ba6980164ad1a7005e88e2ef2b60fe72
inqdesign <163176437+inqdesign@users.noreply.github.com>
2024-04-04 15:43:49 +0200

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -1,12 +1,55 @@
-const swiper = new Swiper(".swiper.is-slider-main", {
-  slidesPreView: "auto",
-  spaceBetween: 16,
-  loop: true,
-  speed: 400,
-  spaceBetween: 100,
+$(".slider-releases_component").each(function (index) {
+  let parentSection = $(this).closest(".section");
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  }
+  const listSwiper = new Swiper($(this).find(".swiper.is-slider-list")[0], {
+    slidesPerView: "auto",
+    speed: 500,
+    loop: true,
+    loopedSlides: 4,
+    direction: "vertical",
+    slideToClickedSlide: true,
+    mousewheel: {
+      forceToAxis: true
+    },
+    pagination: {
+      el: $(this).find(".swiper-bullet-wrapper")[0],
+      bulletActiveClass: "is-active",
+      bulletClass: "swiper-bullet",
+      bulletElement: "button",
+      clickable: true
+    },
+    navigation: {
+      nextEl: $(this).find(".swiper-next")[0],
+      prevEl: $(this).find(".swiper-prev")[0],
+      disabledClass: "is-disabled"
+    },
+    slideActiveClass: "is-active",
+    slideDuplicateActiveClass: "is-active"
+  });
+
+  const photoSwiper = new Swiper($(this).find(".swiper.is-slider-photo")[0], {
+    slidesPerView: 1,
+    speed: 500,
+    loop: true,
+    loopedSlides: 4,
+    keyboard: true,
+    effect: "coverflow",
+    coverflowEffect: {
+      rotate: 0,
+      scale: 0.8,
+      slideShadows: false
+    }
+  });
+
+  listSwiper.controller.control = photoSwiper;
+  photoSwiper.controller.control = listSwiper;
+
+  photoSwiper.on("activeIndexChange", function (e) {
+    let slideColor = $(e.slides[e.activeIndex]).find(".slider-photo_color").text();
+    parentSection.css("background-color", slideColor);
+  });
+
+  $(this).find(".swiper-bullet").each(function (index) {
+    $(this).text(index + 1);
+  });
 });
