b64dd496e10333be0e362546390ea091f0356361
inqdesign <163176437+inqdesign@users.noreply.github.com>
2024-04-04 15:39:28 +0200

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -1,6 +1,12 @@
 const swiper = new Swiper(".swiper.is-slider-main", {
-  slidesPreView: 1,
+  slidesPreView: "auto",
+  spaceBetween: 16,
+  loop: true,
   speed: 400,
   spaceBetween: 100,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   }
 });
